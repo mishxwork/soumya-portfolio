b590e2033191e510daa398db606c9486cc276470
Soumya <soumyamishrawork@gmail.com>
2023-03-29 20:52:31 +0530

    Header Component - Medium and Small Devices

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -9,7 +9,7 @@ const HeaderSocials = () => {
   return (
     <div className='header__socials'>
         <a href="https://www.linkedin.com/in/soumyxmishrx/" target="_blank"><BsLinkedin/></a>
-        <a href="https://www.github.com" target="_blank"><FaGithub/></a>
+        <a href="https://github.com/mishxwork" target="_blank"><FaGithub/></a>
         <a href="https://www.instagram.com/" target="_blank"><SiInstagram/></a>
     </div>
   )
