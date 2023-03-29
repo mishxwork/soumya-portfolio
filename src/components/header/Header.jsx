061a1ed9ef8ba58d70ceb8e46a6758798da70be8
Soumya <soumyamishrawork@gmail.com>
2023-03-30 01:28:25 +0530

    Base Floating Navbar Ready

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,7 +8,7 @@ import HeaderSocials from './HeaderSocials'
 const Header = () => {
   return (
     <header>
-      <div className="container header__container">
+      <div className="container header__container" id='header'>
         <h5>Hello I'm</h5>
         <h1>Soumya Mishra</h1>
         <h5 className="text-light">Fullstack Developer</h5>
@@ -17,9 +17,7 @@ const Header = () => {
         <div className="me">
           <img src={ME} alt="me" />
         </div>
-
         <a href="#contact" className='scroll__down'>Scroll Down</a>
-
       </div>
     </header>
   )
