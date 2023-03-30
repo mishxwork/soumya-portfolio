ae2cffbd91d2979b066a62954f9c2b485dd97cfd
Soumya <soumyamishrawork@gmail.com>
2023-03-30 21:42:31 +0530

    About Component JSX and CSS

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,51 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/meab.jpeg'
+import { FaAward } from 'react-icons/fa'
+import { VscFolderLibrary } from 'react-icons/vsc'
+import { GiPaintBrush } from 'react-icons/gi'
+
+
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon' />
+              <h5>Experience</h5>
+              <small>2+ years Working</small>
+            </article>
+
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon' />
+              <h5>Projects</h5>
+              <small>15+ Completed</small>
+            </article>
+
+            <article className='about__card'>
+              <GiPaintBrush className='about__icon' />
+              <h5>Art</h5>
+              <small>Sketch, Paint, Digital</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, aliquam dolore quisquam neque exercitationem labore porro veniam magnam sequi consequatur? Tempore, voluptas? Eaque dolorem atque quibusdam, pariatur perferendis quia aut.
+          </p>
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+
+    </section>
   )
 }
 
